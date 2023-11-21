aff0d1eb75dfc56937c7571fbcdfe365883ebfb1
MrTango <md@derico.de>
2023-11-21 11:42:54 +0200

    "Add upgrade_step: add accordion_collapsed_view to Folder and Collection"

---
+++ src/collective/accordionviews/upgrades/v1003.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from . import logger
+
+
+from .base import reload_gs_profile
+# from plone import api
+
+
+def upgrade(setup_tool=None):
+    """
+    """
+    logger.info("Running upgrade (Python): add accordion_collapsed_view to Folder and Collection")
+    reload_gs_profile(setup_tool)
